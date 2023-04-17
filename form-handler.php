4fe5033faa1f6997492f9bf17afbf8b473145640
Chesca-A <129781281+Chesca-A@users.noreply.github.com>
2023-04-17 22:43:14 +0800

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,20 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'https://nova-expo.github.io/Nova/Nova_landingpagem.html'
+$email_subject = 'New Submission'
+$email_body = "User Name: $name. \n".
+              "User Email: $visitor_email. \n".
+              "Subject: $subject\n".
+              "User Message: $message. \n".;
+
+$to = 'ap.samanthamay.mangalindan@apecschools.edu.ph'
+$headers = "From: $email_from \r\n"
+$headers . = "Reply-To: $visitor_email \r\n"
+
+mail($to,$email_subject,$email_body,$headers);
+header ("Location: Nova_about.html")
+?>
